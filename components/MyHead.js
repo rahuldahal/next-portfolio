2710a2bf79e99c01ef3b22ba72da08e28fc993e3
Rahul Dahal <rdaahal@gmail.com>
2021-12-06 07:10:57 +0545

    fix: crossOrigin attribute value

--- components/MyHead.js
+++ components/MyHead.js
@@ -18,7 +18,7 @@ export default function MyHead({ title, url, image, description }) {
       <meta property="og:image:height" content="500" />
       <meta name="description" content={description} />
       <link rel="preconnect" href="https://fonts.googleapis.com" />
-      <link rel="preconnect" href="https://fonts.gstatic.com" crossOrigin />
+      <link rel="preconnect" href="https://fonts.gstatic.com" crossOrigin="true" />
       <link href="https://fonts.googleapis.com/css2?family=Roboto:ital,wght@0,300;0,400;0,500;0,700;0,900;1,400&display=swap" rel="stylesheet" />
       <link rel="icon" href="/images/logo.png" />
     </Head>
