7924e6267c1e76f30dcc7270f2ff3ac6b24d9b33
rahuldahal <rdaahal@gmail.com>
2023-06-28 16:14:32 +0545

    fix:ui make the about page visually appealing

--- src/components/GeneralInfo.tsx
+++ src/components/GeneralInfo.tsx
@@ -1,5 +1,3 @@
-'use client';
-
 import Link from 'next/link';
 import classNames from 'classnames';
 import { Inter, Roboto } from '@next/font/google';
@@ -11,34 +9,34 @@ export default function GeneralInfo(): JSX.Element {
   return (
     <section
       id="about"
-      className={classNames('m-auto px-4 pt-40 pb-5', roboto.className)}
+      className={classNames('m-auto px-4 py-40', roboto.className)}
     >
-      <div className="flex justify-around items-center md:flex-row-reverse">
-        <div className="max-w-screen-sm mt-12 md:mt-0 md:p-4 bg-primary-400 text-gray-200 rounded md:shadow-md">
+      <div className="flex flex-col-reverse md:flex-row-reverse justify-center items-center">
+        <div className="max-w-md mx-auto md:ml-8 md:mr-4 md:max-w-lg p-6 bg-primary-400 text-gray-200 rounded shadow-md">
           <h2
             className={classNames(
               'text-3xl text-gray-100 font-bold mb-4',
               inter.className
             )}
           >
-            Building Software Applications with JavaScript.
+            Building Software Applications with JavaScript
           </h2>
-          <p>
+          <p className="text-gray-300">
             My fascination with JavaScript, the core language of web
             development, has led me to become proficient in using JavaScript and
             its associated tools to develop software applications.
           </p>
           <hr className="my-4 border border-gray-300" />
-          <p>
+          <p className="text-gray-300">
             I have been inspired by various industry experts such as{' '}
             <strong>Kyle Simpson</strong>, <strong>Wes Bos</strong>,{' '}
             <strong>Dan Abramov</strong>, <strong>Jake Archibald</strong>,{' '}
             <strong>Surma</strong>, <strong>Brian Holt</strong>,{' '}
-            <strong>Sarah Drasner</strong>, <strong>Kent C. Dodds</strong> and
-            others.{' '}
-            <mark className="bg-primary-200 px-1 text-gray-100">
+            <strong>Sarah Drasner</strong>, <strong>Kent C. Dodds</strong>, and
+            others.
+            <span className="bg-primary-200 text-primary-800 px-1 rounded">
               Being able to understand their thought process
-            </mark>{' '}
+            </span>{' '}
             has contributed significantly to my growth and development as a
             software engineer. My passion for web development has driven me to
             continue learning, honing my skills, and creating applications that
@@ -48,8 +46,9 @@ export default function GeneralInfo(): JSX.Element {
           </p>
         </div>
         <Link
+          className="mt-8 md:mt-0"
           href="https://res.cloudinary.com/rdaahal/image/upload/v1687931296/portfolio/rahul_rzlpol.jpg"
-          title="View full size image"
+          title="View full-size image"
           target="_blank"
         >
           <img
@@ -59,7 +58,7 @@ export default function GeneralInfo(): JSX.Element {
           />
         </Link>
       </div>
-      {/* TODO: add watch portfolio video buton and add a video */}
+      {/* TODO: add watch portfolio video button and add a video */}
     </section>
   );
 }
