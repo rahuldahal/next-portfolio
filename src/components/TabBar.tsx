3b4f7aaac047e60bed6303fd180679025bcb9172
rahuldahal <rdaahal@gmail.com>
2023-07-11 17:00:24 +0545

    feat: add icons on TabBar

--- src/components/TabBar.tsx
+++ src/components/TabBar.tsx
@@ -1,11 +1,13 @@
+import Icon from './Icon';
 import Link from 'next/link';
 import classNames from 'classnames';
 import { navLinks } from '../constants';
+import { iconPaths } from '../constants/iconPaths';
 
 export default function TabBar({ active }): JSX.Element {
   return (
     <div className="md:hidden fixed bottom-0 left-0 w-full bg-primary-500 shadow-lg">
-      <div className="flex items-center justify-between px-4 py-4">
+      <div className="flex items-center justify-between px-4 py-2">
         {navLinks.map((link, index) => {
           const { text, path } = link;
           return (
@@ -13,13 +15,16 @@ export default function TabBar({ active }): JSX.Element {
               key={index}
               href={path}
               className={classNames(
-                'text-gray-200 transition duration-300 ease-in-out hover:bg-primary-600 hover:text-gray-100 px-2 py-2 rounded-md text-base',
+                'text-gray-200 p-4 transition duration-300 ease-in-out hover:bg-primary-600 rounded-md text-base',
                 {
                   'bg-primary-600 text-gray-100': active === text.toLowerCase(),
                 }
               )}
             >
-              {text}
+              <Icon
+                iconPathData={iconPaths[text.toLowerCase()]}
+                className="w-6"
+              />
             </Link>
           );
         })}
