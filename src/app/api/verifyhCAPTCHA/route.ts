22c66deeacfe98af2a2270d05a996800a77164e4
rahuldahal <rdaahal@gmail.com>
2023-07-21 17:18:47 +0545

    fix: hCaptcha verification request

--- src/app/api/verifyhCAPTCHA/route.ts
+++ src/app/api/verifyhCAPTCHA/route.ts
@@ -1,19 +1,28 @@
 export async function POST(req: Request) {
   const { token } = await req.json();
+  const secret = process.env.HCAPTCHA_SECRET_KEY;
+
+  const clientData = {
+    secret,
+    response: token,
+  };
+
+  const formData = new URLSearchParams();
+  for (const [key, value] of Object.entries(clientData)) {
+    formData.append(key, value);
+  }
 
   const hResponse = await fetch('https://hcaptcha.com/siteverify', {
     method: 'POST',
-    headers: { 'Content-Type': 'application/json' },
-    body: JSON.stringify({
-      response: token,
-      secret: process.env.HCAPTCHA_SECRET_KEY,
-    }),
+    body: formData,
   });
 
   const data = await hResponse.json();
 
+  console.log(data);
+
   if (data.success) {
-    const message = JSON.stringify({ verified: true });
+    const message = JSON.stringify({ success: true });
     return new Response(message, {
       status: 200,
     });
